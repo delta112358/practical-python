7cceacd510cb5fc458c5730b1b1c88bb0a560400
delta112358 <tobipolzin@aol.com>
2020-06-23 11:59:35 +0200

    Performed Exercises up to 1.17

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,13 +3,12 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-
-extra_payment_start_month = 5*12+1
-extra_payment_end_month = (extra_payment_start_month-1) + 4*12
+extra_payment_start_month = 5 * 12 + 1
+extra_payment_end_month = (extra_payment_start_month - 1) + 4 * 12
 extra_payment = 1000
 current_month = 1
 while principal > 0:
-    principal = principal * (1+rate/12)
+    principal = principal * (1 + rate / 12)
     cond1 = current_month >= extra_payment_start_month
     cond2 = current_month <= extra_payment_end_month
     if cond1 and cond2:
@@ -20,9 +19,9 @@ while principal > 0:
     if payment_month > principal:
         payment_month = principal
 
-    principal     -= payment_month
-    total_paid    += payment_month
+    principal -= payment_month
+    total_paid += payment_month
     current_month += 1
-    print(current_month, round(total_paid,2),round(principal,2))
-print("Total paid ", round(total_paid,2))
-print("Months", current_month)
+    print(f'{current_month}, {round(total_paid,2)}, {round(principal,2)}')
+print(f"Total paid {round(total_paid,2)}")
+print(f"    Months {current_month}")
